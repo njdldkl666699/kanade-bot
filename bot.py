1e33e82aa88059bf5f0160f2e43e91f08f0ce0ea
南极大陆的可乐 <zhangle666699@qq.com>
2026-06-07 14:04:53 +0800

    fix(wordcloud): :bug: 修复wordcloud插件未加载问题

--- bot.py
+++ bot.py
@@ -28,6 +28,7 @@ def init_nonebot():
     nonebot.load_plugin("nonebot_plugin_localstore")
     nonebot.load_plugin("nonebot_plugin_picstatus_ng")
     nonebot.load_plugin("nonebot_plugin_whateat_pic")
+    nonebot.load_plugin("nonebot_plugin_wordcloud")
 
     nonebot.load_plugins("kanade_bot/plugins")
 
